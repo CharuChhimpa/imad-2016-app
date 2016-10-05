bc7bfec49955315342640260596756ff290a0d06
Charu <201451073@iiitvadodara.ac.in>
2016-10-05 20:14:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,11 +13,21 @@ img.onclick =function() {
 };*/
 //Counter code
 var button = document.getElementById("counter");
-var counter=0;
 button.onclick = function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var counter = reques.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML =counter.toString();
+                
+            }
+        }
+    };
     
-    counter = counter+1;
-    var span = document.getElementById("count");
-    span.innerHTML =counter.toString();
+   request.open('GET','http://charuchhimpa.imad.hasura-app.io/counter',true);
+   request.send(null);
+   
 
 };
