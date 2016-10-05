83a754999f8f4f17e491d080f2ab370375cd703b
Charu <201451073@iiitvadodara.ac.in>
2016-10-05 20:03:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML="New Value";
 
@@ -10,4 +10,14 @@ function moveRight (){
 }
 img.onclick =function() {
     var interval =setInterval(moveRight, 50);
-};
+};*/
+//Counter code
+var button = document.getElementById("counter");
+var counter=0;
+button.onclick = function(){
+    
+    counter = counter+1;
+    var span = document.getElementById("count");
+    span.innerHTML =counter.toString();
+
+}
