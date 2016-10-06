6b95ed59dd5c710ab67adfdd4c947653211e001a
Charu <201451073@iiitvadodara.ac.in>
2016-10-06 07:43:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,8 @@ img.onclick =function() {
 //Counter code
 var button = document.getElementById("counter");
 button.onclick = function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange =function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter =request.responseText;
