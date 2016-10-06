1eff1061554c90659da20546dea42e58eae7b6db
Charu <201451073@iiitvadodara.ac.in>
2016-10-06 07:46:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,14 +12,14 @@ img.onclick =function() {
     var interval =setInterval(moveRight, 50);
 };*/
 //Counter code
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter =request.responseText;
-                var span = document.getElementById("count");
+                var span = document.getElementById('count');
                 span.innerHTML =counter.toString();
             }
         }
